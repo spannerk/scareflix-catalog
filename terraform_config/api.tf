c702c725b999444eebf3553662fefb391f854697
Hannah Gordon <hannahgordon@sky.com>
2023-12-31 10:53:45 +0000

    Add API layer

---
+++ terraform_config/api.tf
@@ -0,0 +1,29 @@
+resource "google_api_gateway_api" "api_gw" {
+  project = var.project_id
+  provider = google-beta
+  api_id = "api-scareflix-cat"
+}
+
+resource "google_api_gateway_api_config" "api_gw" {
+  project = var.project_id
+  provider = google-beta
+  api = google_api_gateway_api.api_gw.api_id
+  api_config_id = "api-scareflix-cat-config"
+
+  openapi_documents {
+    document {
+      path = "spec.yaml"
+      contents = filebase64("../openapi.yaml")
+    }
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "google_api_gateway_gateway" "api_gw" {
+  project = var.project_id
+  provider = google-beta
+  api_config = google_api_gateway_api_config.api_gw.id
+  gateway_id = "api-scareflix-gateway"
+}
--- terraform_config/main.tf
+++ terraform_config/main.tf
@@ -10,12 +10,17 @@ terraform {
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.31.0"
+      version = "~> 5.10.0"
     }
   }
 }
 
 provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+provider "google-beta" {
   project = var.project_id
   region  = var.region
 }
