7882cba165fab44b79dbb1731273c1dc89f7e467
Hannah Gordon <hannahgordon@sky.com>
2024-01-02 16:17:04 +0000

    Fix: change terraform for api to make the lifecyles work

--- terraform_config/api.tf
+++ terraform_config/api.tf
@@ -8,13 +8,17 @@ resource "google_api_gateway_api_config" "api_gw" {
   project = var.project_id
   provider = google-beta
   api = google_api_gateway_api.api_gw.api_id
-  api_config_id = "api-scareflix-cat-config"
+  api_config_id = format("%s-%s","api-scareflix-cat-config",filemd5("../openapi.yaml"))
 
   openapi_documents {
     document {
       path = "spec.yaml"
       contents = filebase64("../openapi.yaml")
     }
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -23,9 +27,5 @@ resource "google_api_gateway_gateway" "api_gw" {
   provider = google-beta
   api_config = google_api_gateway_api_config.api_gw.id
   gateway_id = "api-scareflix-gateway"
-  lifecycle {
-    replace_triggered_by = [
-      google_api_gateway_api_config.api_gw.id
-    ]
-  }
+
 }
