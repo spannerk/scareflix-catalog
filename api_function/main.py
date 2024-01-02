01f2f785fa237734ca94a7f849f8c0cc0cf5ce15
Hannah Gordon <hannahgordon@sky.com>
2024-01-02 10:53:29 +0000

    Remove example function

--- api_function/main.py
+++ api_function/main.py
@@ -2,23 +2,6 @@ import functions_framework
 from google.cloud import firestore_v1
 
 @functions_framework.http
-def hello_get(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-    Note:
-        For more information on how Flask integrates with Cloud
-        Functions, see the `Writing HTTP functions` page.
-        <https://cloud.google.com/functions/docs/writing/http#http_frameworks>
-    """
-    return "Hello World!"
-
-@functions_framework.http
 def get_videos(request):
     db = firestore_v1.Client(project='scareflix', database='scareflix-db')
     col_ref = db.collection("videos")
