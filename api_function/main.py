67b087c0529a77ac5ea73eeee250ff9eef606a06
Hannah Gordon <hannahgordon@sky.com>
2024-01-02 12:28:31 +0000

    Reformat response of the call

--- api_function/main.py
+++ api_function/main.py
@@ -5,4 +5,4 @@ from google.cloud import firestore_v1
 def get_videos(request):
     db = firestore_v1.Client(project='scareflix', database='scareflix-db')
     col_ref = db.collection("videos")
-    return dict([(doc.id, doc.to_dict()) for doc in col_ref.get()])
+    return [{'id':doc.id, 'data': doc.to_dict()} for doc in col_ref.get()]
